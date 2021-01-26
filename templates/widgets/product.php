75d63d9c6618dfa5ea14bb99847ab5589ead07d5
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-26 14:51:25 +0400

    refactor

--- templates/widgets/product.php
+++ templates/widgets/product.php
@@ -11,7 +11,7 @@ $price = wc_get_price_to_display( $product, array( 'price' => $product->get_sale
 ?>
 
 <div
-	class="postpay-widget"
+	id="cashew-widget"
 	data-type="product"
 	data-amount="<?php echo WC_Postpay_Adapter::decimal( $price )->jsonSerialize(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
 	data-currency="<?php echo get_woocommerce_currency(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
