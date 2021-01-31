61a394fe78d858e87ffd3d246f004fe1a8b00696
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-31 15:22:25 +0400

    refactor code

--- assets/js/iframe-lightbox.js
+++ assets/js/iframe-lightbox.js
@@ -182,8 +182,6 @@ function openIFrame() {
 		btn.addEventListener("click", function(e) {
 			e.preventDefault();
 			removeOverlay();
-			showOverlay();
-			openIFrame();
 		});
 
 		[].forEach.call(document.getElementsByClassName("lightbox"), function(el) {
