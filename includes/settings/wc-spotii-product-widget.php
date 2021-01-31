a9b3a6e639e517ab1a251ffa1ea05266b8fd0137
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-31 19:24:38 +0400

    refactor code

--- includes/settings/wc-spotii-product-widget.php
+++ includes/settings/wc-spotii-product-widget.php
@@ -9,8 +9,8 @@ function add_product_widget()
     $instal = $product->get_price();
     $curr = get_woocommerce_currency_symbol();
     $currency = get_woocommerce_currency();
-        echo '<div id="spotii-product-widget">' .
-            '</div><div id="spotii-product-widget-price" style="display:none;">' . $instal . '</div>' .
+        echo '<div id="cashew-widget" data-amount="'.$instal.'" data-currency="' . $currency . '">' .
+            '</div>' .
             '<script>window.spotiiConfig = {targetXPath: [\'#spotii-product-widget-price\'], currency: "' . $currency . '",};</script>' .
             ' <script>(function(w,d,s) {var f=d.getElementsByTagName(s)[0];var a=d.createElement(\'script\');a.async=true;a.src=\'https://s3-eu-west-1.amazonaws.com/cdn.cashewpayments.com/widgets/woocommerce.widget.min.js\';f.parentNode.insertBefore(a,f);}(window, document, \'script\'));</script> ';
 }
