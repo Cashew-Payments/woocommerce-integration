5b130ff14049297f58b7a00a1cc93b2244381bb1
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-31 19:20:15 +0400

    refactor code

--- includes/settings/wc-spotii-product-widget.php
+++ includes/settings/wc-spotii-product-widget.php
@@ -6,15 +6,12 @@ function add_product_widget()
 {
     global $product;
     $th = new WC_Spotii_Gateway_Shop_Now_Pay_Later;
-    $widget_text = $th->widget_text;
     $url = $th->popup_link;
-    $theme = $th->widget_theme;
-    $render = $th->render_path_product;
     $instal = $product->get_price();
     $curr = get_woocommerce_currency_symbol();
     $currency = get_woocommerce_currency();
         echo '<div id="spotii-product-widget">' .
             '</div><div id="spotii-product-widget-price" style="display:none;">' . $instal . '</div>' .
-            '<script>window.spotiiConfig = {targetXPath: [\'#spotii-product-widget-price\'], renderToPath: [\'' . $render . '\'],currency: "' . $currency . '",templateLine:"' . $widget_text . '",theme:"' . $theme . '",minNote:"' . $custom_note_en . '",howItWorksURL : "' . $url . '",};</script>' .
-            ' <script>(function(w,d,s) {var f=d.getElementsByTagName(s)[0];var a=d.createElement(\'script\');a.async=true;a.src=\'https://widget.spotii.me/v1/javascript/priceWidget-en.js\';f.parentNode.insertBefore(a,f);}(window, document, \'script\'));</script> ';
+            '<script>window.spotiiConfig = {targetXPath: [\'#spotii-product-widget-price\'], currency: "' . $currency . '",howItWorksURL : "' . $url . '",};</script>' .
+            ' <script>(function(w,d,s) {var f=d.getElementsByTagName(s)[0];var a=d.createElement(\'script\');a.async=true;a.src=\'https://s3-eu-west-1.amazonaws.com/cdn.cashewpayments.com/widgets/woocommerce.widget.min.js\';f.parentNode.insertBefore(a,f);}(window, document, \'script\'));</script> ';
 }
