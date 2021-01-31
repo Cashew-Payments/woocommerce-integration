81aa394f5efe404657bafb7e91ae3934ae62fef0
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-31 17:22:57 +0400

    refactor code

--- includes/request/wc-spotii-process-payment.php
+++ includes/request/wc-spotii-process-payment.php
@@ -12,11 +12,12 @@ function processPayment($order_id, $th, $type = null, $addon)
     if ($currency == "USD") {
         $total = $total * 3.6730;
     }
-    $min = $th->order_min;
-    $max = $th->order_max;
+    $min = (int)$th->order_min;
+    $max = (int)$th->order_max;
     // Spotii minimum limit 
-    echo $min.' ' .$max. ' ' . $total;
-    if ((int)$total < $min || (int)$total > $max) {
+    $totalConverted = (int)$total;
+    echo 'TEST: '.$totalConverted < $min || $totalConverted > $max;
+    if ($totalConverted < $min || $totalConverted > $max) {
         $errorMin = $lang == 'ar' ? "المبلغ الاجمالي في سلتك أقل من الحد الادنى لاستخدام سبوتي: سبوتي متاح للطلبات بقيمة اعلى من" . $min . " درهم اماراتي أو " . $min . " ريال سعودي. بقليل من التسوق يمكن تقسيم دفعاتك على أربع أقساط خالية من التكاليف الاضافية. " : "You don't quite have enough in your basket: Spotii is available for purchases over AED " . $min . " or SAR " . $min . ". With a little more shopping, you can split your payment over 4 cost-free instalments.";
         error_log("Exception [WP_Error_Spotii] " . $errorMin);
         throw new Exception(__($errorMin));
