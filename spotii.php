fb6856718d7fd9723b0bed9002d4681dc8d9a7b1
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-02-01 07:43:02 +0400

    refactor code

--- spotii.php
+++ spotii.php
@@ -22,49 +22,43 @@ defined('ABSPATH') || exit;
 require __DIR__ . '/includes/settings/wc-spotii-cart-widget.php';
 require __DIR__ . '/includes/settings/wc-spotii-product-widget.php';
 
+add_filter('woocommerce_payment_gateways', 'cashew_add_gateway_class');
 
-/*
- *  spotii add gateway class
- */
-add_filter('woocommerce_payment_gateways', 'spotii_add_gateway_class');
-
-function spotii_add_gateway_class($gateways)
+function cashew_add_gateway_class($gateways)
 {
 
     $gateways[] = 'WC_Cashew_Gateway';
     return $gateways;
 }
-/*
- * Load Spotii Gateway class on plugins_loaded action
- */
-add_action('plugins_loaded', 'spotii_init_gateway_class');
+
+add_action('plugins_loaded', 'cashew_init_gateway_class');
 
 
-function spotii_init_gateway_class()
+function cashew_init_gateway_class()
 {
 
     if (class_exists('WC_Cashew_Gateway') || !class_exists('WC_Payment_Gateway')) {
         return;
     }
 
-    define('WC_SPOTII_DIR_PATH', plugin_dir_path(__FILE__));
+    define('WC_CASHEW_DIR', plugin_dir_path(__FILE__));
     /*
     /* Include files
     */
-    include_once WC_SPOTII_DIR_PATH . 'includes/settings/wc-spotii-gateway-parameters.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/request/wc-spotii-auth.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/settings/wc-spotii-form-fields.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/settings/wc-spotii-validation.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/request/wc-spotii-payload.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/request/wc-spotii-process-payment.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/request/wc-spotii-response-handler.php';
-    include_once WC_SPOTII_DIR_PATH . 'includes/request/wc-spotii-refund.php';
+    include_once WC_CASHEW_DIR . 'includes/settings/wc-spotii-gateway-parameters.php';
+    include_once WC_CASHEW_DIR . 'includes/request/wc-authentication.php';
+    include_once WC_CASHEW_DIR . 'includes/settings/wc-spotii-form-fields.php';
+    include_once WC_CASHEW_DIR . 'includes/settings/wc-spotii-validation.php';
+    include_once WC_CASHEW_DIR . 'includes/request/wc-spotii-payload.php';
+    include_once WC_CASHEW_DIR . 'includes/request/wc-spotii-process-payment.php';
+    include_once WC_CASHEW_DIR . 'includes/request/wc-spotii-response-handler.php';
+    include_once WC_CASHEW_DIR . 'includes/request/wc-spotii-refund.php';
     /*
     * Load cashew Gateway
     */
-    include_once WC_SPOTII_DIR_PATH . '/includes/gateways/class-wc-cashew.php';
+    include_once WC_CASHEW_DIR . '/includes/gateways/class-wc-cashew.php';
     /*
     * Load Spotii function 
     */
-    include_once WC_SPOTII_DIR_PATH . '/includes/wc-spotii-function.php';
+    include_once WC_CASHEW_DIR . '/includes/wc-helper.php';
 }
