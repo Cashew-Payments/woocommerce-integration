40fd4ff499631b00232bb83aa2df46823fdf633d
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-02-01 09:30:34 +0400

    refactor code

--- includes/settings/wc-product-widget.php
+++ includes/settings/wc-product-widget.php
@@ -7,7 +7,7 @@ function add_product_widget()
     global $product;
     $instal = $product->get_price();
     $curr = get_woocommerce_currency_symbol();
-        echo '<div id="cashew-widget" data-amount="'.$instal.'" data-currency="' . $curr . '">' .
+        echo '<div id="cashew-widget" data-language="'.explode('_', get_locale())[0].'" data-amount="'.$instal.'" data-currency="' . $curr . '">' .
             '</div>' .
             ' <script>(function(w,d,s) {var f=d.getElementsByTagName(s)[0];var a=d.createElement(\'script\');a.async=true;a.src=\'https://s3-eu-west-1.amazonaws.com/cdn-dev.cashewpayments.com/widgets/woocommerce.widget.min.js\';f.parentNode.insertBefore(a,f);}(window, document, \'script\'));</script> ';
 }
