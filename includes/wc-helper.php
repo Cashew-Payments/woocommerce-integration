59fab6bd08b43436966d25e0524a99161e6aecc7
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-02-01 08:06:09 +0400

    refactor code

--- includes/wc-helper.php
+++ includes/wc-helper.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * helper functions
+ * Spotii functions
  */
 
 defined('ABSPATH') || exit;
@@ -18,17 +18,48 @@ $lang = get_locale();
  * @param string     $handle Script handle the data will be attached to.
  * @param array|null $params Parameters injected.
  */
-function wc_cashew_script()
+function wc_spotii_script()
 {
+    wp_enqueue_style('spotii-gateway', plugin_dir_url(__FILE__) .  '../assets/css/spotii-checkout.css', true);
     if (is_checkout()) {
-        wp_enqueue_script('cashew-checkout', 'https://s3-eu-west-1.amazonaws.com/cdn-dev.cashewpayments.com/widgets/woocommerce.checkout.min.js', array('jquery'), '0.02', true);
+        wp_enqueue_script('cashew-checkout', 'https://s3-eu-west-1.amazonaws.com/cdn-dev.cashewpayments.com/widgets/woocommerce.checkout.min.js', array('jquery'), '0.01', true);
     }
-    wp_enqueue_script('cashew-checkout', plugin_dir_url(__FILE__) . '../assets/js/cashew-checkout.js', array('jquery'), '0.02', true);
+    wp_enqueue_script('spotii-checkout', plugin_dir_url(__FILE__) . '../assets/js/spotii-checkout.js', array('jquery'), '0.01', true);
     if (is_checkout()) {
         wp_deregister_script('wc-checkout');
-        wp_enqueue_script('wc-checkout', plugin_dir_url(__FILE__) . '../assets/js/woocommerce-checkout.js', array('jquery'), '0.02', true);
+        wp_enqueue_script('wc-checkout', plugin_dir_url(__FILE__) . '../assets/js/woocommerce-checkout.js', array('jquery'), '0.01', true);
     }
     wp_enqueue_script('jquery');
-    wp_localize_script('jquery', 'cashew_ajax', array('ajax_url' => admin_url('admin-ajax.php')));
+    wp_localize_script('jquery', 'spotii_ajax', array('ajax_url' => admin_url('admin-ajax.php')));
 }
-add_action('wp_enqueue_scripts', 'wc_cashew_script', 12);
+add_action('wp_enqueue_scripts', 'wc_spotii_script', 12);
+
+/*
+/* Admin js for hide and show sandbox fields
+*/
+function admin_js()
+{
+?>
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            //Conditional calue 
+            $("input[id*='order_minimum']").on("change paste keyup", function() {
+                if (parseFloat($("input[id*='order_minimum']").val()) < 0) {
+                    if (!$('#error-code-min').length) {
+                        $("#woocommerce_spotii_shop_now_pay_later_order_minimum").parent().parent().append("<p id='error-code-min' style='color:red;'>Please enter a value more than 0 AED</p>");
+                    } else {
+                        $("#error-code-min").show();
+                    }
+                    $(".submit").children(0).prop("disabled", true);
+                } else {
+                    if ($('#error-code-min').length) {
+                        $("#error-code-min").hide();
+                    }
+                    $(".submit").children(0).prop("disabled", false);
+                }
+            });
+        });
+    </script>
+<?php }
+
+add_action('admin_head', 'admin_js');
