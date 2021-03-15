5460a568ff4cc2868298911574724514a3bfb575
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-03-15 23:01:31 +0400

    .

--- includes/settings/wc-cart-widget.php
+++ includes/settings/wc-cart-widget.php
@@ -4,7 +4,7 @@ function add_cart_widget($cart)
 {
     global $product, $woocommerce;
     $th = new WC_Cashew_Gateway;    
-
+echo 'HERE';
     $isSandbox = get_option('woocommerce_cashew_payments_settings')['sandbox'] == 'yes';
     $domain = $isSandbox ? 's3-eu-west-1.amazonaws.com/cdn-sandbox' : 'cdn';
     $price = $product->get_price();
