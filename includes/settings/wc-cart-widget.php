03c0567494d932422e50fabaee5845ec571ccb58
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-03-16 09:12:22 +0400

    place limit check

--- includes/settings/wc-cart-widget.php
+++ includes/settings/wc-cart-widget.php
@@ -7,9 +7,12 @@ function add_cart_widget($cart)
     $isSandbox = get_option('woocommerce_cashew_payments_settings')['sandbox'] == 'yes';
     $domain = $isSandbox ? 's3-eu-west-1.amazonaws.com/cdn-sandbox' : 'cdn';
     $price = $woocommerce->cart->total;
+    $limit = get_option('woocommerce_cashew_payments_settings')['order_maximum'];
     $language = explode('_', get_locale())[0];
     $currency = get_woocommerce_currency_symbol();
-    echo '<div id="cashew-widget" data-language="' . $language . '" data-amount="' . $price . '" data-currency="' . $currency . '">' .
+    if ($price <= $limit) {
+        echo '<div id="cashew-widget" data-language="' . $language . '" data-amount="' . $price . '" data-currency="' . $currency . '">' .
         '</div>' .
-        ' <script>(function(w,d,s) {var f=d.getElementsByTagName(s)[0];var a=d.createElement(\'script\');a.async=true;a.src=\'https://'.$domain.'.cashewpayments.com/widgets/woocommerce.widget.min.js\';f.parentNode.insertBefore(a,f);}(window, document, \'script\'));</script> ';
+        ' <script>(function(w,d,s) {var f=d.getElementsByTagName(s)[0];var a=d.createElement(\'script\');a.async=true;a.src=\'https://'.$domain.'.cashewpayments.com/widgets/woocommerce.cart.min.js\';f.parentNode.insertBefore(a,f);}(window, document, \'script\'));</script> ';
+    }
 }
